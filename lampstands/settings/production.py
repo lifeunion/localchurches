0cc33729094721194c5b6a50fb41af48e2c5758f
Andree Kurniawan <lifeunion@gmail.com>
2017-02-06 16:49:15 -0600

    add allowed hosts

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -5,6 +5,7 @@ import os
 
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
+ALLOWED_HOSTS = ['*']
 DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
@@ -13,9 +14,6 @@ DATABASES['default'] =  dj_database_url.config()
 	
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 try:
     from .local import *
