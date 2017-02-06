987cbedf40abb4b68ed2d021d7ddcdc694cfe12e
Andree Kurniawan <lifeunion@gmail.com>
2017-02-06 17:13:58 -0600

    chg wsgi whitenoise

--- lampstands/wsgi.py
+++ lampstands/wsgi.py
@@ -14,8 +14,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lampstands.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lampstands.settings")
 
-application = get_wsgi_application()
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
