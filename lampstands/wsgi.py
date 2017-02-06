cea9338301de3779b56c60020a856dc07edc40bb
Andree Kurniawan <lifeunion@gmail.com>
2017-02-06 17:04:09 -0600

    chang positions

--- lampstands/wsgi.py
+++ lampstands/wsgi.py
@@ -16,4 +16,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lampstands.settings.dev")
 
 application = get_wsgi_application()
-ALLOWED_HOSTS=['*']
