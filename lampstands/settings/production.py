51cbcb11184e5b75634e56cec198fd5b48034bd7
Andree Kurniawan <lifeunion@gmail.com>
2017-02-06 17:11:06 -0600

    add whitenoise

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -15,6 +15,15 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 try:
     from .local import *
 except ImportError:
--- lampstands/wsgi.py
+++ lampstands/wsgi.py
@@ -12,7 +12,10 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lampstands.settings.dev")
 
 application = get_wsgi_application()
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
