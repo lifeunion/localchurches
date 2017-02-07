82d73104a51a469ea4071d94f213cc019400985b
Andree Kurniawan <lifeunion@gmail.com>
2017-02-06 18:04:14 -0600

    basic homepage model

--- home/models.py
+++ home/models.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, unicode_literals
+from __future__ import unicode_literals
 
 from django.db import models
 
@@ -6,8 +6,9 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
+
 class HomePage(Page):
-     body = RichTextField(blank=True)
+    body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
