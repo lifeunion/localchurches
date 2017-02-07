148c3a18935fcab6e9cd028bdad3d63e75d26dd7
Andree Kurniawan <lifeunion@gmail.com>
2017-02-07 16:32:25 -0600

    queryset to childpages

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,13 @@ class BlogIndexPage(Page):
         FieldPanel('intro', classname="full")
     ]
 
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super(BlogIndexPage, self).get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
