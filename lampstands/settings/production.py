9037e1062eb5d2ba8fb0a8a8c12c5d45c673ca17
Andree Kurniawan <lifeunion@gmail.com>
2017-02-11 12:49:41 -0600

    change mediafiles loc

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -21,7 +21,6 @@ AWS_STORAGE_BUCKET_NAME = "wagtailadminimages"
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-MEDIAFILES_LOCATION - 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
