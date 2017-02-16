f4a8239b6d88e2c5d6d25d2ccf353ff420eb5729
Andree Kurniawan <lifeunion@gmail.com>
2017-02-16 13:05:32 -0600

    back to original

--- lampstands/settings/base.py
+++ lampstands/settings/base.py
@@ -109,7 +109,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                #'wagtail.contrib.settings.context_processors.settings',
+                'wagtail.contrib.settings.context_processors.settings',
                 'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
