3aa03b988fc257427ca0535fcc55cc583cc5fb4f
Andree Kurniawan <lifeunion@gmail.com>
2017-03-12 04:43:19 -0500

    try one entry map

--- static/lampstands/css/src/church_query.js
+++ static/lampstands/css/src/church_query.js
@@ -1,4 +1,12 @@
 $(".firstStyle").ShopLocator({
-   paginationStyle: 1
-   json: "../src/json/markers.json"
+   infoBubble:{
+   visible:true
+   },
+   marker:{
+   latlng:[30.2672, 97.7431],
+   title:"First",
+   street:"2530 South Congress",
+   zip:"78701",
+   city: "Austin"
+   }
 });
