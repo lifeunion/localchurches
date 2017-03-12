28e1a3e8807d3d92bed697ca50380faa9ce5de12
Andree Kurniawan <lifeunion@gmail.com>
2017-03-12 03:49:28 -0500

    clean json

--- static/lampstands/css/shop-locator/church_query.js
+++ static/lampstands/css/shop-locator/church_query.js
@@ -2,10 +2,6 @@ $(".firstStyle").ShopLocator({
    paginationStyle: 1
    json: "json/markers.json"
 );
-1
-2
-3
-4
 $(".firstStyle").ShopLocator({
    paginationStyle: 1
    json: "json/markers.json"
