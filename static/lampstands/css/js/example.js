5bc60f4b11255d51d068e13bb424f154e0e41320
Andree Kurniawan <lifeunion@gmail.com>
2017-03-14 16:48:36 -0500

    add sizes

--- static/lampstands/css/js/example.js
+++ static/lampstands/css/js/example.js
@@ -197,7 +197,7 @@
         $(".secondStyle").ShopLocator({
             pluginStyle: "cosmic",
             paginationStyle: 1,
-            json: "/static/lampstands/css/src/json/markers.json",
+            json: JSLOC,
             infoBubble: {
                 visible: true,
                 arrowPosition: 50,
@@ -206,7 +206,7 @@
                 minWidth: 170,
                 maxWidth: 250
             },
-            markersIcon: markersIcon_scripted,
+            markersIcon: MRK,
             map:{
                 mapStyle: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#7d8d97"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#f2f5f6"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#e6f3d6"},{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#f4d2c5"},{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"color":"#4e4e4e"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#f4f4f4"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#787878"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"hue":"#ff0000"},{"saturation":"-100"},{"lightness":"45"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#cddbe2"}]}]
             },
@@ -222,9 +222,9 @@
                     widthMD: 56,
                     heightBIG: 75,
                     widthBIG: 75,
-                    iconSmall: iconSmall_scripted,
-                    iconMedium: iconMedium_scripted,
-                    iconBig: iconBig_scripted
+                    iconSmall: SM,
+                    iconMedium: MED,
+                    iconBig: BIG
                 }
             },
             sidebar: {
