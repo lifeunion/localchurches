6fea77ec49307d6ae8ba5488ea049bacab2cc2ed
Andree Kurniawan <lifeunion@gmail.com>
2017-03-20 22:54:14 -0500

    overextends

--- lampstands/core/models.py
+++ lampstands/core/models.py
@@ -1087,7 +1087,7 @@ class ChurchPage(Page):
         max_length=255, blank=True,
         help_text='A short summary of when the locality started meeting'
     )
-    address = RichTextField(blank=True)
+    address = models.CharField(max_length=255, blank=True)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     locality_phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=16) # validators should be a list
     locality_fax_number = models.CharField(validators=[phone_regex], blank=True, max_length=16) # validators should be a list
