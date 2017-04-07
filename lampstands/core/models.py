7574be1f56ba591b3fb7e0fe71a9239624a0cd3b
Andree Kurniawan <lifeunion@gmail.com>
2017-04-07 00:18:33 -0500

    chg beliefs to 9 items

--- lampstands/core/models.py
+++ lampstands/core/models.py
@@ -953,7 +953,7 @@ class BeliefsIndexPage(Page):
             beliefs_posts = beliefs_posts.filter(tags__tag__slug=tag)
 
         # Pagination
-        per_page = 8
+        per_page = 9
         page = request.GET.get('page')
         paginator = Paginator(beliefs_posts, per_page)  # Show 8 blog_posts per page
         try:
