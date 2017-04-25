a7b6540d7c3b02942df2b11e8302f04b9b6b7e99
Andree Kurniawan <lifeunion@gmail.com>
2017-04-25 14:43:18 -0500

    form fix

--- lampstands/core/models.py
+++ lampstands/core/models.py
@@ -1586,7 +1586,7 @@ class ChurchEntryFormPage(Page):
         return context
 
     def serve(self, request):
-        form = church_entry_form_class()
+        form = ChurchEntryFormPageForm
         return render(request, 'lampstands/includes/church_form.html', {
             'page': self,
             'form': form,
