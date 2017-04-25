b894579652805788a9801b0ea9d702a31afa4d88
Andree Kurniawan <lifeunion@gmail.com>
2017-04-25 12:44:51 -0500

    fix serializer

--- lampstands/core/forms.py
+++ lampstands/core/forms.py
@@ -4,5 +4,5 @@ from .models import ChurchPage
 class LocalityEntryForm(forms.ModelForm):
     class Meta:
         model = ChurchPage
-        fields = ['url','id','locality_name', 'meeting_address', 'locality_state_or_province', 
+        fields = ['id','locality_name', 'meeting_address', 'locality_state_or_province', 
             'locality_country', 'locality_phone_number', 'locality_email','locality_web']
