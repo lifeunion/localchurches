940cb0d477c7129495c5716bde708cb78e645aea
Andree Kurniawan <lifeunion@gmail.com>
2017-04-27 23:49:03 -0500

    add wagtail api

--- lampstands/core/urls.py
+++ lampstands/core/urls.py
@@ -5,11 +5,24 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from lampstands.core import views
 from django.conf.urls import include
 from .views import LocalitiesList, LocalitiesDetail
+
+from django.contrib import admin
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.contrib.wagtailapi import urls as wagtailapi_urls
+from wagtail.api.v2.router import WagtailAPIRouter
+from wagtail.api.v2.endpoints import PagesAPIEndpoint
+from wagtail.wagtaildocs.api.v2.endpoints import DocumentsAPIEndpoint
+from wagtail.wagtailimages.api.v2.endpoints import ImagesAPIEndpoint
+
+api = WagtailAPIRouter('api')
+api.register_endpoint('pages', PagesAPIEndpoint)
+api.register_endpoint('images', ImagesAPIEndpoint)
+api.register_endpoint('documents', DocumentsAPIEndpoint)
 
 urlpatterns = [
     url(r'^api/', include(wagtailapi_urls)),
-	url(r'^$', views.api_root),
 	url(r'^api-localities/$', views.LocalitiesList.as_view()),
     url(r'^api-localities/(?P<pk>[0-9]+)/$', views.LocalitiesDetail.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -26,4 +39,5 @@ urlpatterns = format_suffix_patterns([
 urlpatterns += [
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^$', views.api_root),
 ]
