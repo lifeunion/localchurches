b23e05e2ecdb2bb0768253254004ff97918c2d5c
Andree Kurniawan <lifeunion@gmail.com>
2017-04-28 17:28:27 -0500

    add drf setting

--- lampstands/core/views.py
+++ lampstands/core/views.py
@@ -17,7 +17,7 @@ from rest_framework import renderers
 from rest_framework.decorators import api_view, detail_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework.render import JSONRenderer
+from rest_framework.renderers import JSONRenderer
 
 #from rest_framework.views import APIView
 #from rest_framework import status, mixins, generics
