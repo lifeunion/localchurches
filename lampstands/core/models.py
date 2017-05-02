c3fec661912499ebc11b4adf7be6b44e66cd5478
lifeunion <lifeunion@gmail.com>
2017-05-02 15:50:28 -0500

    remote json fetch

--- lampstands/core/models.py
+++ lampstands/core/models.py
@@ -1510,7 +1510,7 @@ class MapPage(Page):
         # Filter by tag
         geoinfo_lat = request.GET.get('lat')
         geoinfo_lng = request.GET.get('lng')
-        zoom_deflevel = 2
+        zoom_deflevel = 7
 
         print (geoinfo_lat)
         print (geoinfo_lng)
