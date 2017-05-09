9e44a5557b6d2a78b56b51e5920281e8be171cc6
lifeunion <lifeunion@gmail.com>
2017-05-09 15:03:42 -0500

    clean mappage

--- lampstands/settings/base.py
+++ lampstands/settings/base.py
@@ -94,6 +94,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'wagtail.wagtailcore.middleware.SiteMiddleware',
     'wagtail.wagtailredirects.middleware.RedirectMiddleware',
