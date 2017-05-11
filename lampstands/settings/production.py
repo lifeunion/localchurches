5133216f7151a5f52757803870b7738109b5f6fa
lifeunion <lifeunion@gmail.com>
2017-05-10 23:26:39 -0500

    security

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -37,22 +37,22 @@ COMPRESS_CSS_FILTERS = [
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
 #set S3 as the place to store your files.
-DEFAULT_FILE_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
-STATICFILES_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = os.environ.get(“AWS_ACCESS_KEY_ID”, “”)
 AWS_SECRET_ACCESS_KEY = os.environ.get(“AWS_SECRET_ACCESS_KEY”, “”)
 AWS_STORAGE_BUCKET_NAME = os.environ.get(“AWS_STORAGE_BUCKET_NAME”, “”)
 AWS_QUERYSTRING_AUTH = False //This will make sure that the file URL does not have unnecessary parameters like your access key.
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com’
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
 #static media settings
-STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
-MEDIA_URL = STATIC_URL + ‘media/’
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, “static”), )
-STATIC_ROOT = ‘staticfiles’
-ADMIN_MEDIA_PREFIX = STATIC_URL + ‘admin/’
+STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+MEDIA_URL = STATIC_URL + 'media/'
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = 'static'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 STATICFILES_FINDERS = (
-‘django.contrib.staticfiles.finders.FileSystemFinder’,
-‘django.contrib.staticfiles.finders.AppDirectoriesFinder’,
+'django.contrib.staticfiles.finders.FileSystemFinder',
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
 
