49cef060b5a129c64181557e823539a0f7743cba
lifeunion <lifeunion@gmail.com>
2017-05-10 23:13:26 -0500

    security

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -5,21 +5,22 @@ import os
 
 env = os.environ.copy()
 SECRET_KEY = env['SECRET_KEY']
-ALLOWED_HOSTS = ['safe-cove-64619.herokuapp.com']
-DEBUG = False
+ALLOWED_HOSTS = ['*']
+DEBUG = True
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
-	
+    
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-AWS_STORAGE_BUCKET_NAME = env['S3_BUCKET_NAME']
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+AWS_STORAGE_BUCKET_NAME = "lcstatic"
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 MEDIAFILES_LOCATION="images"
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-#DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
@@ -34,21 +35,6 @@ COMPRESS_CSS_FILTERS = [
     'compressor.filters.cssmin.CSSMinFilter',
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
-
-#AWS_ACCESS_KEY_ID = env['AWS_ACCESS_KEY_ID']
-#AWS_SECRET_ACCESS_KEY = env['AWS_SECRET_ACCESS_KEY']
-
-    # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
-    # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
-    # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
-    # We also use it in the next setting.
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-    # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
-    # you run `collectstatic`).
-#STATICFILES_LOCATION = 'static'
-#STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 try:
     from .local import *
