95eaed93f6a8ea9beddae0f2a0412b915ceadccf
lifeunion <lifeunion@gmail.com>
2017-05-10 23:10:26 -0500

    security

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -3,8 +3,9 @@ from __future__ import absolute_import, unicode_literals
 from .base import *
 import os
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = ['*']
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+ALLOWED_HOSTS = ['safe-cove-64619.herokuapp.com']
 DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
@@ -13,16 +14,17 @@ DATABASES['default'] =  dj_database_url.config()
 	
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-AWS_STORAGE_BUCKET_NAME_MEDIA = 'lcwagtailmedia'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME_MEDIA
-MEDIAFILES_LOCATION="media"
-#MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_STORAGE_BUCKET_NAME = env['S3_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+MEDIAFILES_LOCATION="images"
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+#DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_HOST_USER = env['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = env['SENDGRID_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -33,9 +35,9 @@ COMPRESS_CSS_FILTERS = [
 ]
 COMPRESS_CSS_HASHING_METHOD = 'content'
 
-#AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+#AWS_ACCESS_KEY_ID = env['AWS_ACCESS_KEY_ID']
+#AWS_SECRET_ACCESS_KEY = env['AWS_SECRET_ACCESS_KEY']
+
     # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
     # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
     # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
@@ -44,9 +46,9 @@ COMPRESS_CSS_HASHING_METHOD = 'content'
 
     # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
     # you run `collectstatic`).
-#STATICFILES_LOCATION = "static"
+#STATICFILES_LOCATION = 'static'
 #STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 try:
     from .local import *
