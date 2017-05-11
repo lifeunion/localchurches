8e398f74d87b8e99b5bd8586b5a7e0651d86c2e8
lifeunion <lifeunion@gmail.com>
2017-05-10 22:41:56 -0500

    security

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -13,8 +13,8 @@ DATABASES['default'] =  dj_database_url.config()
 	
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_STORAGE_BUCKET_NAME_MEDIA = "lcwagtailmedia"
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME_MEDIA
 MEDIAFILES_LOCATION="media"
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 #DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
@@ -35,7 +35,7 @@ COMPRESS_CSS_HASHING_METHOD = 'content'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
+AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
     # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
     # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
     # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
