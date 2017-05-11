7ff50386c64789eb1e82380c46e3eb6eef0bf0e0
lifeunion <lifeunion@gmail.com>
2017-05-11 01:05:16 -0500

    setup production heroku

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -15,13 +15,6 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#AWS_STORAGE_BUCKET_NAME = "lcstatic"
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-#MEDIAFILES_LOCATION="images"
-#MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = env['SENDGRID_USERNAME']
