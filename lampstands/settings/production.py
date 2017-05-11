828fd9b14ff89a54afd1b3c612b3c96442db2e41
lifeunion <lifeunion@gmail.com>
2017-05-10 22:54:40 -0500

    security

--- lampstands/settings/production.py
+++ lampstands/settings/production.py
@@ -13,10 +13,10 @@ DATABASES['default'] =  dj_database_url.config()
 	
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-AWS_STORAGE_BUCKET_NAME_MEDIA = "lcwagtailmedia"
+AWS_STORAGE_BUCKET_NAME_MEDIA = 'lcwagtailmedia'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME_MEDIA
 MEDIAFILES_LOCATION="media"
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+#MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
