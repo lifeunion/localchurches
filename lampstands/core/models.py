e2bd03b47aef9b5d75e99f3abb753f6aef7917e6
lifeunion <lifeunion@gmail.com>
2017-07-07 18:25:40 -0500

    change establishment to cities in home page search

--- lampstands/core/models.py
+++ lampstands/core/models.py
@@ -1525,6 +1525,7 @@ class MapPage(Page):
         # Filter by tag
         geoinfo_lat = request.GET.get('lat')
         geoinfo_lng = request.GET.get('lng')
+        zoom_deflevel = 9
 
         print (geoinfo_lat)
         print (geoinfo_lng)
