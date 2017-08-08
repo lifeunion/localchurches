d638914b9869fd48c533d487ee536c6817867041
lifeunion <lifeunion@gmail.com>
2017-08-07 22:15:50 -0500

    decrease zoom level

--- lampstands/core/models.py
+++ lampstands/core/models.py
@@ -1526,7 +1526,7 @@ class MapPage(Page):
         # Filter by tag
         geoinfo_lat = request.GET.get('lat')
         geoinfo_lng = request.GET.get('lng')
-        zoom_deflevel = 12
+        zoom_deflevel = 11
 
         print (geoinfo_lat)
         print (geoinfo_lng)
