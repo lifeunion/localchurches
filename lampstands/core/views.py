53b7becfbefa05cda3eba311282569be767b31ce
lifeunion <lifeunion@gmail.com>
2018-01-10 16:15:35 -0600

    modify filter serializer

--- lampstands/core/views.py
+++ lampstands/core/views.py
@@ -52,7 +52,7 @@ class LocalitiesDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class LocalitiesBook(generics.ListCreateAPIView):
-    queryset = ChurchPage.objects.filter(locality_country.code = "US")
+    queryset = ChurchPage.objects.filter(locality_name = "Austin")
     serializer_class = LocalitiesSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
