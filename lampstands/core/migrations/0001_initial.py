9d9133b3a4b954f303839c98637c1986cdcaa5df
lifeunion <lifeunion@gmail.com>
2026-01-21 13:45:25 -0500

    Fix get_root_collection_id in migration for Wagtail 6.4
    wagtail.models.get_root_collection_id doesn't exist in Wagtail 6.4.
    Added a helper function that uses Collection.get_first_root_node().id
    for migration compatibility.

--- lampstands/core/migrations/0001_initial.py
+++ lampstands/core/migrations/0001_initial.py
@@ -20,6 +20,12 @@ import wagtail.images.models
 import wagtail.search.index
 
 
+def get_root_collection_id():
+    """Get the root collection ID for migrations compatibility."""
+    from wagtail.models import Collection
+    return Collection.get_first_root_node().id
+
+
 class Migration(migrations.Migration):
 
     initial = True
@@ -459,7 +465,7 @@ class Migration(migrations.Migration):
                 ('focal_point_height', models.PositiveIntegerField(blank=True, null=True)),
                 ('file_size', models.PositiveIntegerField(editable=False, null=True)),
                 ('credit', models.CharField(blank=True, max_length=255)),
-                ('collection', models.ForeignKey(default=wagtail.models.get_root_collection_id, on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtailcore.Collection', verbose_name='collection')),
+                ('collection', models.ForeignKey(default=get_root_collection_id, on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtailcore.Collection', verbose_name='collection')),
                 ('tags', taggit.managers.TaggableManager(blank=True, help_text=None, through='taggit.TaggedItem', to='taggit.Tag', verbose_name='tags')),
                 ('uploaded_by_user', models.ForeignKey(blank=True, editable=False, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL, verbose_name='uploaded by user')),
             ],
