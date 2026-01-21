b726b90c67ae43a92fabbccfbbd722217888ff5a
lifeunion <lifeunion@gmail.com>
2026-01-21 15:17:06 -0500

    Add database migrations to build script
    The database tables for Wagtail 6.4 don't exist yet. Running
    migrations during the build process will create all necessary tables.

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Run database migrations
+python manage.py migrate --no-input
+
 # Collect static files
 python manage.py collectstatic --no-input
