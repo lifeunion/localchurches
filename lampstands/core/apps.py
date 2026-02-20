66783b83cdbf6dece54499d624016ec55c74f244
lifeunion <lifeunion@gmail.com>
2026-02-20 00:17:30 -0500

    Implement custom page lock handling in LocalChurchesCoreConfig
    - Introduced a patched version of the Page.get_lock method to return None for unsaved pages, preventing unnecessary WorkflowState queries.
    - Updated the ready method in LocalChurchesCoreConfig to apply the patch, enhancing the application's handling of page locks.

--- lampstands/core/apps.py
+++ lampstands/core/apps.py
@@ -1,7 +1,19 @@
 from django.apps import AppConfig
 
 
+def _patched_page_get_lock(self):
+    """Return None when page has no pk (unsaved), avoiding WorkflowState query with object_id='None'."""
+    if self.pk is None:
+        return None
+    return _patched_page_get_lock._original(self)
+
+
 class LocalChurchesCoreConfig(AppConfig):
     name = 'lampstands.core'
     label = 'lampstands'
     verbose_name = "Local Churches"
+
+    def ready(self):
+        from wagtail.models import Page
+        _patched_page_get_lock._original = Page.get_lock
+        Page.get_lock = _patched_page_get_lock
